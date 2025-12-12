6cc347e6553978cc31d0080e2a3cf507b58a5ac9
Adrien VET <adrien.vet@ensta-paris.fr>
2025-12-12 16:12:54 +0100

    added info relative to fork when loading the module

--- src/HMatrices.jl
+++ src/HMatrices.jl
@@ -1,5 +1,7 @@
 module HMatrices
 
+@info "Loading forked HMatrices.jl -> from https://github.com/BEMforFracture/HMatrices.jl"
+
 const PROJECT_ROOT = pkgdir(HMatrices)
 
 using StaticArrays
@@ -11,10 +13,10 @@ using Distributed
 using Base.Threads
 using SparseArrays
 
-const AdjOrMat = Union{Matrix,Adjoint{<:Any,<:Matrix}}
+const AdjOrMat = Union{Matrix, Adjoint{<:Any, <:Matrix}}
 
 """
-    getblock!(block,K,irange,jrange)
+	getblock!(block,K,irange,jrange)
 
 Fill `block` with `K[i,j]` for `i ∈ irange`, `j ∈ jrange`, where `block` is of
 size `length(irange) × length(jrange)`.
@@ -24,30 +26,30 @@ method can be overloaded for better performance if e.g. a vectorized way of
 computing a block is available.
 """
 function getblock!(out, K, irange_, jrange_)
-    irange = irange_ isa Colon ? axes(K, 1) : irange_
-    jrange = jrange_ isa Colon ? axes(K, 2) : jrange_
-    for (jloc, j) in enumerate(jrange)
-        for (iloc, i) in enumerate(irange)
-            out[iloc, jloc] = K[i, j]
-        end
-    end
-    return out
+	irange = irange_ isa Colon ? axes(K, 1) : irange_
+	jrange = jrange_ isa Colon ? axes(K, 2) : jrange_
+	for (jloc, j) in enumerate(jrange)
+		for (iloc, i) in enumerate(irange)
+			out[iloc, jloc] = K[i, j]
+		end
+	end
+	return out
 end
 
 function getblock!(out, Kadj::Adjoint, irange_, j::Int)
-    getblock!(transpose(out), parent(Kadj), j:j, irange_)
-    return out .= conj.(out)
+	getblock!(transpose(out), parent(Kadj), j:j, irange_)
+	return out .= conj.(out)
 end
 
 """
-    use_threads()::Bool
+	use_threads()::Bool
 
 Default choice of whether threads will be used throughout the package.
 """
 use_threads() = true
 
 """
-    use_global_index()::Bool
+	use_global_index()::Bool
 
 Default choice of whether operations will use the global indexing system
 throughout the package.
@@ -69,22 +71,22 @@ include("lu.jl")
 include("cholesky.jl")
 
 export ClusterTree,
-    CardinalitySplitter,
-    DyadicSplitter,
-    GeometricSplitter,
-    GeometricMinimalSplitter,
-    HyperRectangle,
-    # abstract types
-    AbstractKernelMatrix,
-    # types
-    HMatrix,
-    KernelMatrix,
-    StrongAdmissibilityStd,
-    WeakAdmissibilityStd,
-    PartialACA,
-    TSVD,
-    # functions
-    compression_ratio,
-    assemble_hmatrix
+	CardinalitySplitter,
+	DyadicSplitter,
+	GeometricSplitter,
+	GeometricMinimalSplitter,
+	HyperRectangle,
+	# abstract types
+	AbstractKernelMatrix,
+	# types
+	HMatrix,
+	KernelMatrix,
+	StrongAdmissibilityStd,
+	WeakAdmissibilityStd,
+	PartialACA,
+	TSVD,
+	# functions
+	compression_ratio,
+	assemble_hmatrix
 
 end
